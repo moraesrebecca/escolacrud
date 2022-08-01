2b503875f3c753e74ed54a55b9e19681dfa6563e
Rebecca Moraes <rgabrielle@ciandt.com>
2022-08-01 14:18:22 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/ciandt/escolateste/EscolatesteApplication.java
@@ -0,0 +1,13 @@
+package com.ciandt.escolateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolatesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolatesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciandt/escolateste/EscolatesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciandt.escolateste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolatesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
